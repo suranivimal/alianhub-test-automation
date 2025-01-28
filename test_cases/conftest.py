225d2e75583d713ac8236dc8d93992bf560f65f6
Surani Vimal <vimal.surani@aliansoftware.com>
2025-01-28 12:04:56 +0530

    Modify code

--- test_cases/conftest.py
+++ test_cases/conftest.py
@@ -5,18 +5,18 @@ from _pytest.config import hookimpl
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-# Configure Chrome options globally for headless mode
+# Configure Chrome options globally for regular (non-headless) mode
 chrome_options = Options()
 chrome_options.add_argument("--disable-infobars")
-chrome_options.add_argument("start-maximized")
+chrome_options.add_argument("start-maximized")  # Ensure the browser window is maximized
 chrome_options.add_argument("--disable-extensions")
-chrome_options.add_argument("--headless")  # Run in headless mode
+# Remove --headless argument for UI visibility
+# chrome_options.add_argument("--headless")  # Commented out to show UI
 chrome_options.add_argument("--no-sandbox")  # Disable sandbox (required for some environments like Docker)
 chrome_options.add_argument("--disable-gpu")  # Disable GPU acceleration
 chrome_options.add_argument("--remote-debugging-port=9222")  # Remote debugging port
 chrome_options.add_argument("--disable-dev-shm-usage")  # Helps to avoid "DevToolsActivePort file doesn't exist" error
 chrome_options.add_argument("--disable-software-rasterizer")  # Avoid issues related to software rasterizer
-chrome_options.add_argument("start-maximized")  # Ensure the browser window is maximized
 
 # Handle Chrome prefs for notifications and media
 chrome_options.add_experimental_option(
@@ -39,7 +39,7 @@ def setup(browser, request):
 
         # Initialize Chrome WebDriver with the options
         driver = webdriver.Chrome(options=chrome_options)
-        print("Launching chrome browser in headless mode.........")
+        print("Launching chrome browser with UI.........")
 
         # Cleanup temporary user data directory after test run
         def cleanup():
@@ -50,12 +50,12 @@ def setup(browser, request):
         request.addfinalizer(cleanup)
 
     elif browser == 'firefox':
-        driver = webdriver.Firefox(options=chrome_options)  # Add headless options for Firefox if needed
-        print("Launching firefox browser in headless mode.........")
+        driver = webdriver.Firefox(options=chrome_options)  # Add regular (non-headless) options for Firefox if needed
+        print("Launching firefox browser with UI.........")
 
     else:
         driver = webdriver.Chrome(options=chrome_options)
-        print("Launching default browser (chrome) in headless mode.........")
+        print("Launching default browser (chrome) with UI.........")
 
     yield driver
     driver.quit()
